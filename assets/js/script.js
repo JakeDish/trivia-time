ebad21a8dbce740a6f32ac66dac37dd05bc8076c
dirtydenny <dallen7@socal.rr.com>
2023-03-09 10:39:49 -0800

    clean out junk

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-const trivApiRoot = "https://the-trivia-api.com/api/questions?"
+const trivaApiRoot = "https://the-trivia-api.com/api/questions?"
     // categories=film_and_tv,general_knowledge,history,music,science&limit=10&region=US&difficulty=medium
 
     https://the-trivia-api.com/api/questions?categories=film_and_tv,general_knowledge,history,music,science,arts_and_literature,food_and_drink,geography,society_and_culture,sport_and_leisure&limit=10&region=US&difficulty=medium
@@ -14,14 +14,30 @@ const geography="geography";
 const society_and_culture="society_and_culture";
 const sport_and_leisure="sport_and_leisure";
 
-function renderQuizData() {
+var search_term=
+
+const fetchQuizData = (search_term) => {
+    
+    fetch(
+      `${trivaApiRoot}=${search_term}&limit=10&region=US&difficulty=${difficulty}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        // T
+       
+
+      }}
+
+// function renderQuizData() {}
 
 
-}
+
 
 document.getElementById("startGame").addEventListener("submit", function (event) {
   
   
+  
+  
   // start game here...
-  renderQuizData()
+  //renderQuizData()
 })
