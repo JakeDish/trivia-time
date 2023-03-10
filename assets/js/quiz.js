55f406f9910f3376a111c93868ea58e78e415ee0
Eric <ericnguyen23@gmail.com>
2023-03-10 15:34:11 -0800

    Add logic to go to quiz page, save name and score to local storage

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,59 +1,64 @@
-var wrongA
-var rightA
-var shuffled
-var rightAnswers = []
-var rightGuesses = 0
-var wrongGuesses = 0
-var questionNum = 1
+var wrongA;
+var rightA;
+var shuffled;
+var rightAnswers = [];
+var rightGuesses = 0;
+var wrongGuesses = 0;
+var questionNum = 1;
 const container = document.getElementById("container");
 const questionTitle = document.getElementById("question-title");
 
+// Set array for playerStats and set existing data from localStorage
+var playerStats = [];
+playerStats = JSON.parse(localStorage.getItem("players"));
 
-document.getElementById("answer-buttons").addEventListener('click', function (event) {    
-  var guess = event.target.textContent;
-  if (rightGuesses + wrongGuesses < 9) {
-    if (rightAnswers.includes(guess)) {
-      console.log('right')
-      rightGuesses++
+document
+  .getElementById("answer-buttons")
+  .addEventListener("click", function (event) {
+    var guess = event.target.textContent;
+    if (rightGuesses + wrongGuesses < 9) {
+      if (rightAnswers.includes(guess)) {
+        // console.log("right");
+        rightGuesses++;
+      } else {
+        // console.log("wrong");
+        wrongGuesses++;
       }
-     else {
-      console.log ('wrong')
-      wrongGuesses++
-      }
-     displayQuestion()
-     questionNum++
-     console.log(rightGuesses, wrongGuesses)
+      displayQuestion();
+      questionNum++;
+      // console.log(rightGuesses, wrongGuesses);
+    } else {
+      // console.log("game over");
+      endQuiz();
     }
-  else {
-    console.log("game over")
-    endQuiz()
-    }  
-    })    
-  
+  });
 
 function displayQuestion() {
-fetch('https://the-trivia-api.com/api/questions?limit=1')
-  .then((response) => response.json())
-  .then((data) => {
-  for (let i = 0; i < data.length; i++) {;
-    let wrongA = data[i].incorrectAnswers
-    let rightA = data[i].correctAnswer
-    rightAnswers.push(rightA)
-    var choices = wrongA.concat(rightA)
-    let shuffled = choices.sort((a,b) => 0.5 - Math.random());
-    document.getElementById("question-content").innerText = data[i].question;
-    document.getElementById("a1").innerText = shuffled[0]
-    document.getElementById("a2").innerText = shuffled[1]
-    document.getElementById("a3").innerText = shuffled[2]
-    document.getElementById("a4").innerText = shuffled[3]
-    document.getElementById("question-number").innerText = questionNum
+  fetch("https://the-trivia-api.com/api/questions?limit=1")
+    .then((response) => response.json())
+    .then((data) => {
+      for (let i = 0; i < data.length; i++) {
+        let wrongA = data[i].incorrectAnswers;
+        let rightA = data[i].correctAnswer;
+        rightAnswers.push(rightA);
+        var choices = wrongA.concat(rightA);
+        let shuffled = choices.sort((a, b) => 0.5 - Math.random());
+        document.getElementById("question-content").innerText =
+          data[i].question;
+        document.getElementById("a1").innerText = shuffled[0];
+        document.getElementById("a2").innerText = shuffled[1];
+        document.getElementById("a3").innerText = shuffled[2];
+        document.getElementById("a4").innerText = shuffled[3];
+        document.getElementById("question-number").innerText = questionNum;
       }
-    })
-  }
+    });
+}
 
 function endQuiz() {
   container.classList.add("hidden");
   questionTitle.classList.add("hidden");
+  playerStats.push(rightGuesses);
+  localStorage.setItem("players", JSON.stringify(playerStats));
 }
 
-  displayQuestion ()  
+displayQuestion();
