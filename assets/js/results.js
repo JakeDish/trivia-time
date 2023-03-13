5b9d2214ceb56256cc6f0d98bfc556ba9108d250
Tenshi541 <angelzavaleta541@gmail.com>
2023-03-13 10:23:21 -0700

    display giphy

--- assets/js/results.js
+++ assets/js/results.js
@@ -1,29 +1,34 @@
-var apiKey = "vkWBAa2LfhiieA9v1VVSP5pwtU8MRTB4";
-var searchTerm = "goodjob"
-var score = 6
+function displayScore(score) {
 
-fetch(`https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${searchTerm}&rating=pg-13`)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) { 
-  display_image(data.message);
-    console.log(data);
-  });
-  function display_image(image_url){
-     document.getElementById("image").src = image_url
+
+  var searchTerm = ""
+  if (score > 7) {
+    result = "winner"
+    searchTerm = "winner"
+  } else if (score > 5) {
+    result = "almsot had it"
+    searchTerm = "almost had it"
+  } else {
+    result = "loser"
+    searchTerm = "loser"
   }
-if (score > 6) {
-} else {
-  result = "Loser";
-}
-if (score > 7) {
-} else {
-  result = "almost";
-}
-if (score < 7) {
-} else {
-  result = "YAY";
+  var apiKey = "vkWBAa2LfhiieA9v1VVSP5pwtU8MRTB4";
+
+
+
+  fetch(`https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${searchTerm}&rating=pg-13`)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (result) {
+      // display_image(data.message);
+      var image_url = result.data[0].images.downsized_medium.url;
+      display_image(image_url)
+    });
+  function display_image(image_url) {
+    console.log(image_url)
+    document.getElementById("image").src = image_url
+  }
 }
 // timer and game tracker
 var timePara = document.getElementById("timePara");
