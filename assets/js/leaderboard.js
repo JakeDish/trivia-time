53cb849a8bbc30fe54e5720180d18ee53dfdb075
Jake Disharoon <jakedisharoon@gmail.com>
2023-03-13 11:45:23 -0700

    clear leaderboard function

--- assets/js/leaderboard.js
+++ assets/js/leaderboard.js
@@ -12,7 +12,7 @@ var leaderContainer = document.getElementById("leader-container");
 for (var i = 0; i < playerStatsUpdated.length; i++) {
   var cardEl = document.createElement("div");
   cardEl.innerHTML = `
-    <div>
+    <div class="removable-card">
       <div class="uk-card uk-card-default uk-card-body ">
         <h2 class="uk-card-title uk-margin-bottom">${playerStatsUpdated[i].name}</h2>
         <h3 class="uk-card-title uk-text-success uk-text-bolder">${playerStatsUpdated[i].score}/10</h3>
@@ -21,3 +21,11 @@ for (var i = 0; i < playerStatsUpdated.length; i++) {
   `;
   leaderContainer.append(cardEl);
 }
+
+document.getElementById("clear-leaderboard-btn").addEventListener("click", function (event){
+  var cards = document.getElementsByClassName("removable-card")
+  while(cards.length > 0){
+    cards[0].parentNode.removeChild(cards[0])
+  }
+  localStorage.clear()
+})
